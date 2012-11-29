885914e33c38918782eed4cf41518d32ec6f0c5b
Tyler Lartonoix <larttyler@live.com>
2012-11-29 08:52:05 -0500

    Added a basic player manager

---
+++ src/me/dbstudios/solusrpg/managers/PlayerManager.java
@@ -0,0 +1,59 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package me.dbstudios.solusrpg.managers;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+import java.util.logging.Level;
+import me.dbstudios.solusrpg.SolusRpg;
+import me.dbstudios.solusrpg.entities.RpgPlayer;
+import me.dbstudios.solusrpg.exceptions.RpgPlayerConfigException;
+import org.bukkit.entity.Player;
+
+/**
+ *
+ * @author Tyler Lartonoix
+ */
+public class PlayerManager {
+    private static final Map<UUID, RpgPlayer> players = new HashMap<>();
+    
+    public static boolean add(UUID uuid, Player player) {
+	try {
+	    players.put(uuid, new RpgPlayer(player));
+	    
+	    return true;
+	} catch (RpgPlayerConfigException e) {
+	    SolusRpg.log(Level.SEVERE, "Could not insatance player data for '{0}' Reason: {1}.", player.getName(), e.getMessage());
+	    player.kickPlayer("An error occurred while reading your player data, or creating you a new profile. Please contact an administrator.");
+	    
+	    return false;
+	}
+    }
+    
+    public static RpgPlayer get(UUID uuid) {
+	return players.get(uuid);
+    }
+    
+    public static RpgPlayer get(Player player) {
+	return PlayerManager.get(player.getUniqueId());
+    }
+    
+    public static boolean exists(UUID uuid) {
+	return players.containsKey(uuid);
+    }
+    
+    public static boolean exists(Player player) {
+	return PlayerManager.exists(player.getUniqueId());
+    }
+    
+    public static void remove(UUID uuid) {
+	players.remove(uuid);
+    }
+    
+    public static void remove(Player player) {
+	PlayerManager.remove(player.getUniqueId());
+    }
+}
