f77e5f85c19019eb4ee330587c511cf00de32cfa
Tyler Lartonoix <larttyler@live.com>
2012-12-06 13:22:07 -0500

    Continued adding basic stock event handlers

--- src/me/dbstudios/solusrpg/event/listeners/RpgStockListener.java
+++ src/me/dbstudios/solusrpg/event/listeners/RpgStockListener.java
@@ -5,9 +5,14 @@
 
 package me.dbstudios.solusrpg.event.listeners;
 
+import java.util.HashMap;
+import java.util.Map;
 import me.dbstudios.solusrpg.entities.RpgPlayer;
 import me.dbstudios.solusrpg.entities.conf.PermitNode;
 import me.dbstudios.solusrpg.event.block.RpgBlockBreakEvent;
+import me.dbstudios.solusrpg.event.block.RpgBlockPlaceEvent;
+import me.dbstudios.solusrpg.event.player.RpgPlayerInteractEvent;
+import me.dbstudios.solusrpg.managers.PhraseManager;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
@@ -22,7 +27,43 @@ public class RpgStockListener implements Listener {
 	RpgPlayer player = ev.getPlayer();
 	
 	if (!player.isAllowed(PermitNode.BREAK, ev.getBlockName())) {
+	    ev.setCancelled(true);
 	    
+	    Map<String, String> args = new HashMap<>();
+	    
+	    args.put("item", ev.getBlockName().replace('_', ' ').toLowerCase());
+	    
+	    if (PhraseManager.phraseExists("player.break-deny"))
+		player.sendEventMessage(PhraseManager.getPhrase("player.break-deny"), args);
+	}
+    }
+    
+    @EventHandler(priority = EventPriority.LOW)
+    public void onRpgBlockPlace(RpgBlockPlaceEvent ev) {
+	RpgPlayer player = ev.getPlayer();
+	
+	if (!player.isAllowed(PermitNode.PLACE, ev.getBlockName())) {
+	    ev.setCancelled(true);
+	    
+	    Map<String, String> args = new HashMap<>();
+	    
+	    args.put("item", ev.getBlockName().replace('_', ' ').toLowerCase());
+	    
+	    if (PhraseManager.phraseExists("player.place-deny"))
+		player.sendEventMessage(PhraseManager.getPhrase("player.place-deny"), args);
+	}
+    }
+    
+    @EventHandler(priority = EventPriority.LOW)
+    public void onRpgPlayerInteract(RpgPlayerInteractEvent ev) {
+	RpgPlayer player = ev.getPlayer();
+	
+	if (!player.isAllowed(PermitNode.USE, ev.getItemName())) {
+	    ev.setCancelled(true);
+	    
+	    Map<String, String> args = new HashMap<>();
+	    
+	    args.put("item", ev.getItemName().replace('_', ' ').toLowerCase());
 	}
     }
 }
