544ea5282a55ef7688a6471efd94293f51260038
Tyler Lartonoix <larttyler@live.com>
2012-12-06 09:02:51 -0500

    Began adding basic stock event handlers

--- src/me/dbstudios/solusrpg/event/listeners/RpgStockListener.java
+++ src/me/dbstudios/solusrpg/event/listeners/RpgStockListener.java
@@ -5,6 +5,11 @@
 
 package me.dbstudios.solusrpg.event.listeners;
 
+import me.dbstudios.solusrpg.entities.RpgPlayer;
+import me.dbstudios.solusrpg.entities.conf.PermitNode;
+import me.dbstudios.solusrpg.event.block.RpgBlockBreakEvent;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 
 /**
@@ -12,5 +17,12 @@ import org.bukkit.event.Listener;
  * @author Tyler Lartonoix
  */
 public class RpgStockListener implements Listener {
-
+    @EventHandler(priority = EventPriority.LOW)
+    public void onRpgBlockBreak(RpgBlockBreakEvent ev) {
+	RpgPlayer player = ev.getPlayer();
+	
+	if (!player.isAllowed(PermitNode.BREAK, ev.getBlockName())) {
+	    
+	}
+    }
 }
