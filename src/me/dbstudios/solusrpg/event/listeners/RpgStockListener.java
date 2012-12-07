af38c71dbc409de36bbf73c6446cc2037507afef
Tyler Lartonoix <larttyler@live.com>
2012-12-07 09:00:58 -0500

    Continued adding stock event handlers

--- src/me/dbstudios/solusrpg/event/listeners/RpgStockListener.java
+++ src/me/dbstudios/solusrpg/event/listeners/RpgStockListener.java
@@ -25,45 +25,48 @@ public class RpgStockListener implements Listener {
     @EventHandler(priority = EventPriority.LOW)
     public void onRpgBlockBreak(RpgBlockBreakEvent ev) {
 	RpgPlayer player = ev.getPlayer();
-	
+
 	if (!player.isAllowed(PermitNode.BREAK, ev.getBlockName())) {
 	    ev.setCancelled(true);
-	    
+
 	    Map<String, String> args = new HashMap<>();
-	    
+
 	    args.put("item", ev.getBlockName().replace('_', ' ').toLowerCase());
-	    
+
 	    if (PhraseManager.phraseExists("player.break-deny"))
 		player.sendEventMessage(PhraseManager.getPhrase("player.break-deny"), args);
 	}
     }
-    
+
     @EventHandler(priority = EventPriority.LOW)
     public void onRpgBlockPlace(RpgBlockPlaceEvent ev) {
 	RpgPlayer player = ev.getPlayer();
-	
+
 	if (!player.isAllowed(PermitNode.PLACE, ev.getBlockName())) {
 	    ev.setCancelled(true);
-	    
+
 	    Map<String, String> args = new HashMap<>();
-	    
+
 	    args.put("item", ev.getBlockName().replace('_', ' ').toLowerCase());
-	    
+
 	    if (PhraseManager.phraseExists("player.place-deny"))
 		player.sendEventMessage(PhraseManager.getPhrase("player.place-deny"), args);
 	}
     }
-    
+
     @EventHandler(priority = EventPriority.LOW)
     public void onRpgPlayerInteract(RpgPlayerInteractEvent ev) {
 	RpgPlayer player = ev.getPlayer();
-	
+
 	if (!player.isAllowed(PermitNode.USE, ev.getItemName())) {
 	    ev.setCancelled(true);
-	    
+
 	    Map<String, String> args = new HashMap<>();
-	    
+
 	    args.put("item", ev.getItemName().replace('_', ' ').toLowerCase());
+
+	    if (PhraseManager.phraseExists("player.use-deny"))
+		player.sendEventMessage(PhraseManager.getPhrase("player.use-deny"), args);
 	}
     }
 }
