ef16e557deaf8df35b375b68d6d1516e4759befd
Tyler Lartonoix <larttyler@live.com>
2012-12-07 09:00:18 -0500

    Added the ability to change RpgClass
    This change will be necessary for class change commands

--- src/me/dbstudios/solusrpg/entities/RpgPlayer.java
+++ src/me/dbstudios/solusrpg/entities/RpgPlayer.java
@@ -21,91 +21,98 @@ import org.getspout.spoutapi.player.SpoutPlayer;
  * @author Tyler Lartonoix
  */
 public class RpgPlayer {
-    private final RpgClass rpgClass;
     private final SpoutPlayer basePlayer;
-    
+
+    private RpgClass rpgClass;
+
     public RpgPlayer(Player p) throws RpgPlayerConfigException {
         this.basePlayer = SpoutManager.getPlayer(p);
-	
+
 	File f = new File(Directories.DATA + p.getName().substring(0, 2).toLowerCase() + File.separator + p.getName().toLowerCase() + ".yml");
-	
+
 	if (!f.exists()) {
 	    (new File(f.getPath().substring(0, f.getPath().lastIndexOf(File.separator)))).mkdirs();
-	    
+
 	    try {
 		f.createNewFile();
 	    } catch (IOException e) {
 		throw new RpgPlayerConfigException("Could not create player data file.");
 	    }
 	}
-	
+
 	FileConfiguration conf = YamlConfiguration.loadConfiguration(f);
 	String cl = conf.getString("player.class", null);
-	
+
 	if (ClassManager.exists(cl))
 	    this.rpgClass = ClassManager.getClass(cl);
 	else
 	    this.rpgClass = ClassManager.DEFAULT_CLASS;
     }
-    
+
     public String getName() {
 	return basePlayer.getName();
     }
-    
+
     public String getDisplayName() {
 	return basePlayer.getDisplayName();
     }
-    
+
     public RpgPlayer setDisplayName(String name) {
 	basePlayer.setDisplayName(name);
-	
+
 	return this;
     }
-    
+
     public RpgPlayer setTitle(String name) {
 	basePlayer.setTitle(name);
-	
+
 	return this;
     }
-    
+
     public RpgPlayer setPlayerListName(String name) {
 	basePlayer.setPlayerListName(name);
-	
+
 	return this;
     }
-    
+
     public RpgPlayer sendMessage(String msg) {
 	for (ChatColor c : ChatColor.values())
 	    msg = msg.replaceAll("(?i)\\{" + c.name() + "\\}", c.toString());
-	
+
 	msg = msg.replaceAll("(?i)\\{player-name\\}", this.getName());
 	msg = msg.replaceAll("(?i)\\{display-name\\}", this.getDisplayName());
 	msg = msg.replaceAll("(?i)\\{class\\}", rpgClass.getName());
-	
+
 	basePlayer.sendMessage(msg);
-	
+
 	return this;
     }
-    
+
     public RpgPlayer sendMessage(String msg, Object... args) {
 	return this.sendMessage(Util.format(msg, args));
     }
-    
+
     public RpgPlayer sendEventMessage(String msg, Map<String, String> args) {
 	for (String key : args.keySet())
 	    msg = msg.replaceAll("(?i)\\{" + key + "\\}", args.get(key));
-	
+
 	return this.sendMessage(msg);
     }
-    
+
     public SpoutPlayer getBasePlayer() {
 	return this.basePlayer;
     }
-    
+
     public RpgClass getRpgClass() {
 	return this.rpgClass;
     }
-    
+
+    public RpgPlayer setRpgClass(RpgClass cl) {
+	this.rpgClass = cl;
+
+	return this;
+    }
+
     public boolean isAllowed(PermitNode node, String item) {
 	return rpgClass.isAllowed(node, item);
     }
