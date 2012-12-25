c1a36ba919ba3c61d4c59d4a0c142bd1fbce460f
Tyler Lartonoix <larttyler@live.com>
2012-12-25 12:23:15 -0500

    Added getWeapon convenience method

--- src/me/dbstudios/solusrpg/event/player/RpgPlayerDamageByPlayerEvent.java
+++ src/me/dbstudios/solusrpg/event/player/RpgPlayerDamageByPlayerEvent.java
@@ -8,6 +8,8 @@ package me.dbstudios.solusrpg.event.player;
 import me.dbstudios.solusrpg.entities.RpgPlayer;
 import me.dbstudios.solusrpg.managers.PlayerManager;
 import me.dbstudios.solusrpg.util.DamageType;
+import me.dbstudios.solusrpg.util.Util;
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.entity.EntityDamageEvent.DamageCause;
 
@@ -17,14 +19,20 @@ import org.bukkit.event.entity.EntityDamageEvent.DamageCause;
  */
 public class RpgPlayerDamageByPlayerEvent extends RpgPlayerDamageEvent {
     private final RpgPlayer damager;
+    private final String weapon;
 
     public RpgPlayerDamageByPlayerEvent(Player player, Player damager, DamageCause cause, int damage, DamageType type) {
 	super(player, cause, damage, type);
 
 	this.damager = PlayerManager.get(damager);
+        this.weapon = Util.getItemName(damager.getItemInHand().getType(), damager.getItemInHand().getData().getData());
     }
 
     public RpgPlayer getDamager() {
 	return this.damager;
+    }
+
+    public String getWeapon() {
+        return this.weapon;
     }
 }
