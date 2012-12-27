df23952e3e22a5d9532a6a216e9876517d674258
Tyler Lartonoix <larttyler@live.com>
2012-12-27 09:59:06 -0500

    Began writing command handling

--- src/me/dbstudios/solusrpg/commands/RpgInfoCommand.java
+++ src/me/dbstudios/solusrpg/commands/RpgInfoCommand.java
@@ -5,8 +5,15 @@
 
 package me.dbstudios.solusrpg.commands;
 
+import java.util.List;
+import me.dbstudios.solusrpg.entities.RpgPlayer;
+import me.dbstudios.solusrpg.entities.conf.RpgClass;
+import me.dbstudios.solusrpg.managers.ClassManager;
+import me.dbstudios.solusrpg.managers.PlayerManager;
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 /**
  *
@@ -14,7 +21,21 @@ import org.bukkit.command.CommandSender;
  */
 public class RpgInfoCommand {
     public static boolean onCommand(CommandSender sender, Command command, String... args) {
+        List<Player> potentialMatches = Bukkit.matchPlayer(args[1]);
 
+        if (!potentialMatches.isEmpty()) {
+            RpgPlayer target = PlayerManager.get(potentialMatches.get(0));
+
+            // TODO: Display player stats
+        } else {
+            List<RpgClass> potentialClasses = ClassManager.matchClass(args[1]);
+
+            if (!potentialClasses.isEmpty()) {
+                RpgClass target = potentialClasses.get(0);
+
+                // TODO: Display class stats
+            }
+        }
 
 	return false;
     }
