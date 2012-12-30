b657b4a96a3cffe6cf96cbb0a58ca2db48d163f8
Tyler Lartonoix <larttyler@live.com>
2012-12-30 18:21:57 -0500

    Added check for connected players on disable

--- src/me/dbstudios/solusrpg/SolusRpg.java
+++ src/me/dbstudios/solusrpg/SolusRpg.java
@@ -5,11 +5,16 @@ import java.io.File;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import me.dbstudios.solusrpg.commands.RpgCommandExecutor;
+import me.dbstudios.solusrpg.entities.RpgPlayer;
+import me.dbstudios.solusrpg.entities.conf.ItemGroups;
 import me.dbstudios.solusrpg.event.listeners.EventDistributor;
 import me.dbstudios.solusrpg.event.listeners.RpgStockListener;
 import me.dbstudios.solusrpg.managers.ClassManager;
 import me.dbstudios.solusrpg.managers.PhraseManager;
+import me.dbstudios.solusrpg.managers.PlayerManager;
 import me.dbstudios.solusrpg.util.Directories;
+import me.dbstudios.solusrpg.util.OutputFormatter;
+import me.dbstudios.solusrpg.util.SimpleFormatter;
 import me.dbstudios.solusrpg.util.Util;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -31,6 +36,11 @@ public class SolusRpg extends JavaPlugin {
         if (!f.exists())
             Util.extract("/resources/config.yml", f);
 
+        f = new File(Directories.CONFIG + "item_groups.yml");
+
+        if (!f.exists())
+            Util.extract("/resources/item_groups.yml", f);
+
         f = new File(Directories.CONFIG + "player_info_format.dat");
 
         if (!f.exists())
@@ -46,6 +56,8 @@ public class SolusRpg extends JavaPlugin {
         if (!f.exists())
             Util.extract("/resources/class_list_format.dat", f);
 
+        OutputFormatter.registerFormatter(new SimpleFormatter());
+
         Bukkit.getPluginManager().registerEvents(new EventDistributor(), this);
         Bukkit.getPluginManager().registerEvents(new RpgStockListener(), this);
 
@@ -53,7 +65,20 @@ public class SolusRpg extends JavaPlugin {
 
 	SolusRpg.log(Level.INFO, "Loaded {0} class{1}.", ClassManager.size(), ClassManager.size() != 1 ? "es" : "");
 	SolusRpg.log(Level.INFO, "Loaded {0} phrase{1}.", PhraseManager.size(), PhraseManager.size() != 1 ? "s" : "");
+        SolusRpg.log(Level.INFO, "Loaded {0} item group{1}.", ItemGroups.size(), ItemGroups.size() != 1 ? "s" : "");
 	SolusRpg.log(Level.INFO, "SolusRpg enabled in {0} milliseconds.", System.currentTimeMillis() - start);
+    }
+
+    public void onDisable() {
+        if (PlayerManager.size() > 0) {
+            SolusRpg.log(Level.WARNING, "{0} player{1} {2} still connected when the shutdown command was given, and will be saved before shut down can continue.", PlayerManager.size(), PlayerManager.size() != 1 ? "s" : "",
+                    PlayerManager.size() != 1 ? "were" : "was");
+            
+            for (RpgPlayer p : PlayerManager.getOnlinePlayers())
+                PlayerManager.remove(p.getBasePlayer());
+        }
+
+        SolusRpg.log(Level.INFO, "SolusRpg disabled.");
     }
 
     public static SolusRpg getInstance() {
