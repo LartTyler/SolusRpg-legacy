1688bb8941773dc356f11bda4e830ac9ea30c94e
Tyler Lartonoix <larttyler@live.com>
2012-12-30 18:28:13 -0500

    Began adding basic formatting commands
    Then stopped when I realized this would be a pain to implement now

--- src/me/dbstudios/solusrpg/util/SimpleFormatter.java
+++ src/me/dbstudios/solusrpg/util/SimpleFormatter.java
@@ -5,10 +5,20 @@
 
 package me.dbstudios.solusrpg.util;
 
+import me.dbstudios.solusrpg.entities.conf.RpgClass;
+import me.dbstudios.solusrpg.managers.ClassManager;
+
 /**
  *
  * @author Tyler Lartonoix
  */
 public class SimpleFormatter implements Formatter {
-    
+    public static String classListNL(String prefix) {
+        String val = "";
+
+        for (RpgClass cl : ClassManager.matchClass(".*"))
+            val += prefix + cl.getName();
+
+        return val;
+    }
 }
