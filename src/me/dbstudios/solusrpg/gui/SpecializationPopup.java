8261b492261a0179e199169fb4bc9937089ddcfd
Tyler Lartonoix <larttyler@live.com>
2013-01-03 11:25:12 -0500

    Removed all setAuto and setFixed calls
    Hopefully, starting with a "clean" slate tonight will be enough to
    finally get this working.

--- src/me/dbstudios/solusrpg/gui/SpecializationPopup.java
+++ src/me/dbstudios/solusrpg/gui/SpecializationPopup.java
@@ -27,7 +27,7 @@ public class SpecializationPopup extends GenericPopup {
         Container c = new GenericContainer();
         int screenWidth = owner.getBasePlayer().getMainScreen().getWidth(), screenHeight = owner.getBasePlayer().getMainScreen().getHeight();
 
-        c.setAuto(false).setFixed(true).setAnchor(WidgetAnchor.TOP_LEFT).setWidth(screenWidth - 20).setHeight(screenHeight - 20).setX(10).setY(10).setAutoDirty(true);
+        c.setAnchor(WidgetAnchor.TOP_LEFT).setWidth(screenWidth - 20).setHeight(screenHeight - 20).setX(10).setY(10).setAutoDirty(true);
 
         SolusRpg.log(Level.INFO, "Spec. tree container: width = {0}, height = {1}, x = {2}, y = {3}", c.getWidth(), c.getHeight(), c.getX(), c.getY());
 
@@ -42,7 +42,7 @@ public class SpecializationPopup extends GenericPopup {
             else
                 children[i].setX(children[i - 1].getWidth() + 5);
 
-        this.setFixed(true).setWidth(screenWidth).setHeight(screenHeight).setX(0).setY(0).setAnchor(WidgetAnchor.TOP_LEFT);
+        this.setWidth(screenWidth).setHeight(screenHeight).setX(0).setY(0).setAnchor(WidgetAnchor.TOP_LEFT);
         this.attachWidget(SolusRpg.getInstance(), c);
 
         SolusRpg.log(Level.INFO, "SpecializationPopup: width = {0}, height = {1}, x = {2}, y = {3}", this.getWidth(), this.getHeight(), this.getX(), this.getY());
@@ -53,7 +53,7 @@ public class SpecializationPopup extends GenericPopup {
 
         Container mc = new GenericContainer();
 
-        mc.setAlign(WidgetAnchor.TOP_LEFT).setAuto(false).setAutoDirty(true).setAnchor(WidgetAnchor.TOP_LEFT);
+        mc.setAlign(WidgetAnchor.TOP_LEFT).setAutoDirty(true).setAnchor(WidgetAnchor.TOP_LEFT);
 
         List<Specialization> specList = new ArrayList<>();
 
@@ -84,7 +84,7 @@ public class SpecializationPopup extends GenericPopup {
 
         SolusRpg.log(Level.INFO, "\t'{0}' tree found to be {1} deep and {2} wide.", rootSpec.getUniqueName(), deep, wide);
 
-        mc.setAuto(false).setFixed(true).setWidth(32 * wide + 5 * (wide - 1)).setHeight(48 * deep + 6 * (deep - 1)).setAnchor(WidgetAnchor.TOP_LEFT);
+        mc.setWidth(32 * wide + 5 * (wide - 1)).setHeight(48 * deep + 6 * (deep - 1)).setAnchor(WidgetAnchor.TOP_LEFT);
 
         SolusRpg.log(Level.INFO, "\t'{0}' tree found to have a width of {1} and a height of {2}.", rootSpec.getUniqueName(), mc.getWidth(), mc.getHeight());
 
@@ -110,15 +110,15 @@ public class SpecializationPopup extends GenericPopup {
                 Label levelLabel = new GenericLabel(spec.getLevel(owner) + "/" + spec.getMaxLevel());
                 Button levelUpButton = new LevelUpButton("Level Up", spec, levelLabel);
 
-                c.setAuto(false).setFixed(true).setAnchor(WidgetAnchor.CENTER_CENTER).setWidth(32).setHeight(48).setX(((sectionWidth * pos) - (int)Math.floor((double)sectionWidth / 2.0)) + (5 * (pos - 1))).setY(54 * depth);
+                c.setAnchor(WidgetAnchor.CENTER_CENTER).setWidth(32).setHeight(48).setX(((sectionWidth * pos) - (int)Math.floor((double)sectionWidth / 2.0)) + (5 * (pos - 1))).setY(54 * depth);
 
                 SolusRpg.log(Level.INFO, "\t\t\tPlacing Spec. container at x = {0} and y = {1}.", c.getX(), c.getY());
 
                 c.addChildren(g, levelUpButton, levelLabel);
 //                image.setAnchor(WidgetAnchor.TOP_LEFT).setHeight(32).setWidth(32).setX(0).setY(0);
-                g.setFixed(true).setAnchor(WidgetAnchor.TOP_LEFT).setHeight(32).setWidth(32).setX(0).setY(0);
-                levelUpButton.setAuto(false).setFixed(true).setAnchor(WidgetAnchor.TOP_LEFT).setHeight(8).setWidth(32).setX(0).setY(0);
-                levelLabel.setAuto(false).setTextColor(new Color(255, 255, 255)).setShadow(true).setAlign(WidgetAnchor.TOP_RIGHT).setFixed(true).setAnchor(WidgetAnchor.TOP_LEFT).setHeight(8).setWidth(30).setX(0)
+                g.setAnchor(WidgetAnchor.TOP_LEFT).setHeight(32).setWidth(32).setX(0).setY(0);
+                levelUpButton.setAnchor(WidgetAnchor.TOP_LEFT).setHeight(8).setWidth(32).setX(0).setY(0);
+                levelLabel.setTextColor(new Color(255, 255, 255)).setShadow(true).setAlign(WidgetAnchor.TOP_RIGHT).setAnchor(WidgetAnchor.TOP_LEFT).setHeight(8).setWidth(30).setX(0)
                         .setY(20).setAutoDirty(true).setPriority(RenderPriority.Low);
 
 //                c.addChildren(image, levelUpButton, levelLabel);
